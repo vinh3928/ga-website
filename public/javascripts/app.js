e8db248431f36959c51383074ddc1e00780e5482
Vinh Nguyen <vnguyen.cheme@gmail.com>
2015-07-28 21:34:19 -0600

    created larger population

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -68,7 +68,7 @@ Population.prototype.generation = function () {
     }
   }
 
-  var newPopulation = new Population(11, 8, newPool);
+  var newPopulation = new Population(11, 12, newPool);
   return newPopulation;
 
 };
@@ -228,7 +228,7 @@ function advertBox (population) {
 orbit.get("/data/population.json", function () {
   specimen = JSON.parse(this.response);
   if (specimen === false) {
-    var population = new Population(11, 8);
+    var population = new Population(11, 12);
     orbit.post("/data/newpopulation", population, function () {
       window.location = "/home";
     });
@@ -241,7 +241,7 @@ orbit.get("/data/population.json", function () {
         var gene = new Gene(data[i].population.code, data[i].population.fitness);
         array.push(gene);
       }
-      var population = new Population(11, 8, array);
+      var population = new Population(11, 12, array);
       var newPopulation = population.generation();
       orbit.post("/data/newpopulation", newPopulation, function () {
         window.location = "/home";
